0835996671e25afad14593451a01da23dae6c74b
vyang2968 <v.yang2968@gmail.com>
2026-03-16 00:52:14 -0400

    remove placeholder

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -275,7 +275,7 @@ export default function Home() {
           data-show={hasMessages ? "messages" : "greeting"}
         >
           {hasMessages ? (
-            <div>Some data</div>
+            <div></div>
           ) : (
             <p className={styles.greeting}>Hey! How can I help?</p>
           )}
