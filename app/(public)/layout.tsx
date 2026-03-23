59f375987d526652705f0b7dabdcecfd017f5d05
vyang2968 <v.yang2968@gmail.com>
2026-03-23 14:22:08 -0400

    add badge stuff

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/components/ui/badge";
 import HeaderButtons from "./components/header-buttons/header-buttons";
 import Image from "next/image";
 
@@ -16,16 +17,25 @@ export default function PublicLayout({
         className="bg-background sticky top-0 z-10 flex w-full items-center justify-between p-4"
         data-header
       >
-        <div className="flex items-center justify-center gap-2">
-          <Image
-            src="/images/logo.svg"
-            alt="AI PI Logo"
-            className="h-8 w-auto"
-            width={32}
-            height={32}
-          />
-          <h2 className="text-lg font-bold">AI PI</h2>
+        <div className="flex flex-row items-center gap-x-2">
+          <div className="flex items-center justify-center gap-2">
+            <Image
+              src="/images/logo.svg"
+              alt="AI PI Logo"
+              className="h-8 w-auto"
+              width={32}
+              height={32}
+            />
+            <h2 className="text-lg font-bold">AI PI</h2>
+          </div>
+          <Badge
+            className="h-4 border-amber-500 text-xs text-amber-500"
+            variant="outline"
+          >
+            Beta
+          </Badge>
         </div>
+
         <HeaderButtons className="flex items-center gap-x-1" />
       </header>
       <main
